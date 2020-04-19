bd2d2807aabd980122006e818b3627c1b6cbe47b
Prashant Pokhriyal <prashant.pokhriyal09@gmail.com>
2020-04-19 16:08:24 -0700

    Special Modes Completed

--- backend/player.py
+++ backend/player.py
@@ -1,12 +1,15 @@
-from AI import choose_column
+import random
+from AI import choose_column, choose_worst_column
 from color import print_red, print_blue
 
 class Player:
 
-    def __init__(self, name, color, difficulty=6):
+    def __init__(self, name, color, difficulty=6, reverse=False, mayhem=False):
         self.name = name
         self.color = color
         self.difficulty = difficulty
+        self.reverse = reverse
+        self.mayhem = mayhem
 
     def players_turn(self):
         statement = "It is " + self.name + "'s turn\n"
@@ -17,6 +20,16 @@ class Player:
 
 
     def choose_input(self, board, board_size):
-        if (self.name == 'AI'):
+        if self.name == 'AI' and not self.reverse:
             return str(choose_column(board, board_size, self.difficulty) + 1)
+        elif self.name == 'AI' and self.reverse:
+            return str(choose_worst_column(board, board_size, self.difficulty) + 1)
+        elif self.name == 'AI' and self.mayhem:
+            rand_selection = random.randint(1, board_size+1)
+            return str(rand_selection)
+        elif self.name != 'AI' and self.mayhem:
+            rand_num = random.randint(1, 1000)
+            print('A secret number has been selected from 1 to 1000.')
+            player_input = input('Select a MOD number between 1 and ' + str(board_size) + ': ')
+            return str((rand_num % int(player_input)) + 1)
         return input('Select a column from 1 to ' + str(board_size) + ': ')
